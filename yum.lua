6ebe7ce6eb9c229a01a6743ed3c7d4b6a4d4e59f
= <=>
2015-09-18 23:41:46 +0100

    attempt at github backing

--- yum.lua
+++ yum.lua
@@ -1,8 +1,10 @@
-PROTOCOL_CHANNEL = 137
-LISTEN_CHANNEL = os.getComputerID()
-os.loadAPI("api/redString")
-os.loadAPI("api/sovietProtocol")
 os.loadAPI('/.sys/yggdrasil/yggdrasil')
+os.loadAPI('/.sys/red_string')
+
+YUM_ERRORS = {
+	NO_ERROR = 0,
+	PACKAGE_NOT_EXISTS = 1
+}
 
 if not yggdrasil.namespace_exists('yum') then
 	yggdrasil.namespace_create('yum')
@@ -10,54 +12,83 @@ end
 
 local i_yum = yggdrasil.namespace_open('yum')
 
-sovietProtocol.setDebugLevel(0)
 local args = {...}
 
 local yum  = nil
 
 local method = args[1]
 local package = args[2]
-local file = args[3]
+local sources = {
+	'GRSSR'}
 
-local function installFile(location, file)
-	print("installing "..location)
-	local f = io.open(location, "w")
-	f:write(file)
-	f:close()
+local Package = {}
+
+local function Package:new(package_name)
+	local obj = {
+		name = package_name,
+		version = nil,
+		dependancies = {},
+		files = {},
+		base_url = nil
+	}
+	setmetatable(obj, self)
+	self.__index = self
+	obj.base_url = "https://raw.githubusercontent.com/GRSSR/".. package_name .."/master/"
+	return obj
 end
 
-local function install_package(name)
-	print('installing '..name)
-	local file = {}
-	yum:send("install", name)
-	replyChannel, response = yum:listen()
-	if response.method == "package_list" then
-		for line in response.body:gmatch("[^\r\n]+") do
-			local parsed = redString.split(line)
-			if parsed[1] == "package" then
-				install_package(parsed[2])
-			elseif parsed[1] == "alias" then
-				if NONIX then
-					print('Setting up alias '..parsed[2])
-					nonix.register_alias(parsed[2], parsed[3])
-				end
-			else
-				file.name = parsed[1]
-				file.installLocation = parsed[2]
-				file.hash = parsed[3]
-				file.package = name
-				yum:send("install", file.package, file.name)
-				replyChannel, response = yum:listen()
-				if response.method == "file" then
-					installFile(response.id, response.body)
-				end
-			end
-		end
-	else
-		print('not a package')
-		error()
+
+
+function Package:get_dependancies()
+	for _, dependancy in pairs(self.dependancies) do
+		local dependancy_pkg = Package:new(dependancy)
+		dependancy_pkg:install()
 	end
 end
+
+local function Package:retrieve_index()
+	local index = http.get(self.base_url .. "index")
+	if a == nil then
+		return YUM_ERRORS.PACKAGE_NOT_EXISTS
+	end
+
+	local index_table = textutils.unserialize(index:readAll())
+	self.dependancies = index_table.dependancies
+	self.files = index_table.files
+	self.version = index_table.version
+
+	return YUM_ERRORS.NO_ERROR
+end
+
+function Package:install()
+	-- get our index file
+	err = self:retrieve_index()
+	if err == YUM_ERRORS.PACKAGE_NOT_EXISTS then 
+		return err
+	end
+
+	-- recursively deal with dependancies
+	self:get_dependancies()
+
+	for _, file_info in pairs(self.files) do
+		print("getting ".. file_name)
+		local file = http.get(self.base_url .. file.repo_location)
+		local local_destination = io.open(file.install_location, 'w')
+		local_destination:write(file:readAll())
+		local_destination:close()
+	end
+
+end
+
+function Package:update()
+end
+
+function Package:uninstall()
+end
+
+local function get_package_info(package)
+end
+
 
 local function help()
 	print("usage yum install|list|replicate")
@@ -69,62 +100,16 @@ local function installHelp()
 	error()
 end
 
-local function parsePackage(response)
-	local files = {}
-	for line in response.body:gmatch("[^\r\n]+") do
-		local parsed = redString.split(line)
-		files[#files+1] = {}
-		files[#files].name = parsed[1]
-		files[#files].installLocation = parsed[2]
-		files[#files].has = parsed[3] 
-	end
-	return files
-end
-
-
-for side, modem in pairs(sovietProtocol.findModems()) do 
-	local possible = sovietProtocol.Protocol:new("yum", PROTOCOL_CHANNEL, LISTEN_CHANNEL, side)
-	if possible:hello() then
-		yum = possible
-		break;
-	else
-		possible:tearDown()
-	end
-end
-
-if not yum:hello() then
-	print("Yum server is asleep :(")
-	error()
-end
-
 if method == "install" then
 	if not package then installHelp() end
-	install_package(package)
+	local pkg = Package:new(package)
+	err = pkg:install()
+	if err then
+		print("oops")
+	end
 
 elseif method ==  "list" then
-	yum:send("list", package)
-	local reply, response = yum:listen()
-	local fileList = parsePackage(response)
-
-	if package then
-		print("files in package "..package..":")
-	else
-		print("Packages available: ")
-	end
-	for i, file in pairs(fileList) do
-		print (file.name)
-	end
-elseif method == "replicate" then
-	yum:send("replicate", "list")
-	local replyChannel, response = yum:listen()
-	if response.method == "file_list" then
-		print(response.body)
-		for fileName in response.body:gmatch("[^\r\n]+") do
-			yum:send("replicate", fileName)
-			local reply, response = yum:listen()
-			installFile(response.id, response.body)
-		end
-	end
+	print("NYI")
 else
 	help()
 end
